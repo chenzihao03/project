736ecc8e7fc085415616a94366ebdf166137b719
陈梓豪 <chenzihao03@gmail.com>
2020-04-01 23:56:03 +0800

    侧边栏引入elementUi

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './common/router';
+import element from './common/element';
 import Vant from 'vant';
 import 'vant/lib/index.css';
 import '@/common/styles/index.css';
@@ -8,8 +9,10 @@ import '@/common/styles/index.css';
 import 'font-awesome/css/font-awesome.min.css';
 import Mui from 'vue-awesome-mui';
 
+
 Vue.use(Mui);
 Vue.use(Vant);
+Vue.use(element);
 Vue.config.productionTip = false;
 
 new Vue({
